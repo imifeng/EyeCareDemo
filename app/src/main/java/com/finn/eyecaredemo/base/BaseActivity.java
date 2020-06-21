c339c6102227a872134e23b033048c4ec8497a70
finn <992639429@qq.com>
2020-06-21 23:33:15 +0800

    Optimize code

--- app/src/main/java/com/finn/eyecaredemo/base/BaseActivity.java
+++ app/src/main/java/com/finn/eyecaredemo/base/BaseActivity.java
@@ -12,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.finn.eyecaredemo.common.Constant;
 import com.finn.eyecaredemo.utils.DialogUtil;
+import com.finn.eyecaredemo.utils.EyeCareColorUtil;
 
 import butterknife.ButterKnife;
 
@@ -21,6 +22,8 @@ import butterknife.ButterKnife;
  */
 public abstract class BaseActivity extends AppCompatActivity {
 
+    private FrameLayout eyeCareView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,9 +31,9 @@ public abstract class BaseActivity extends AppCompatActivity {
         if (view != 0) {
             setContentView(view);
         }
-
         ButterKnife.bind(this);
 
+        //添加一层浮层（初始化）
         initEye();
 
         initView();
@@ -40,17 +43,13 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     public abstract int getLayoutId();
 
-
     protected void initView(){
-
     }
 
     protected void initData(){
-
     }
 
     protected void initEvent(){
-
     }
 
     protected abstract void onBack();
@@ -67,17 +66,6 @@ public abstract class BaseActivity extends AppCompatActivity {
         return super.onKeyDown(keyCode, event);
     }
 
-    /**
-     * Set font size, language
-     * @return
-     */
-    @Override
-    public Resources getResources() {
-        //Set font size, language
-        return super.getResources();
-    }
-
-
     @Override
     protected void onRestart() {
         super.onRestart();
@@ -88,13 +76,11 @@ public abstract class BaseActivity extends AppCompatActivity {
         }
     }
 
-    private FrameLayout view;
-
     /**
      * 添加护眼模式浮层
      */
     private void initEye() {
-        view = new FrameLayout(this);
+        eyeCareView = new FrameLayout(this);
         if (Constant.IS_EYE_CARE_OPEN){
             openEye();
         }else {
@@ -106,21 +92,30 @@ public abstract class BaseActivity extends AppCompatActivity {
                 | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE;
         params.width = WindowManager.LayoutParams.MATCH_PARENT;
         params.height = WindowManager.LayoutParams.MATCH_PARENT;
-        getWindow().addContentView(view, params);
+        getWindow().addContentView(eyeCareView, params);
     }
 
     /**
      * 开启护眼模式
      */
     public void openEye() {
-        view.setBackgroundColor(DialogUtil.getFilterColor(30));
+        eyeCareView.setBackgroundColor(EyeCareColorUtil.getFilterColor(30));
     }
 
     /**
      * 关闭护眼模式
      */
     public void closeEye() {
-        view.setBackgroundColor(Color.TRANSPARENT);
+        eyeCareView.setBackgroundColor(Color.TRANSPARENT);
     }
 
+    /**
+     * Set font size, language
+     * @return
+     */
+    @Override
+    public Resources getResources() {
+        //Set font size, language
+        return super.getResources();
+    }
 }
--- app/src/main/java/com/finn/eyecaredemo/utils/DialogUtil.java
+++ app/src/main/java/com/finn/eyecaredemo/utils/DialogUtil.java
@@ -29,7 +29,7 @@ public class DialogUtil {
 
         FrameLayout eye_content = layout.findViewById(R.id.eye_content);
         if (Constant.IS_EYE_CARE_OPEN) {
-            eye_content.setBackgroundColor(getFilterColor(30));
+            eye_content.setBackgroundColor(EyeCareColorUtil.getFilterColor(30));
         } else {
             eye_content.setBackgroundColor(Color.TRANSPARENT);
         }
@@ -50,25 +50,6 @@ public class DialogUtil {
         });
 
         mAlertDialog.show();
-    }
-
-    /**
-     * 过滤蓝光
-     *
-     * @param blueFilterPercent 蓝光过滤比例[10-30-80]
-     */
-    public static int getFilterColor(int blueFilterPercent) {
-        int realFilter = blueFilterPercent;
-        if (realFilter < 10) {
-            realFilter = 10;
-        } else if (realFilter > 80) {
-            realFilter = 80;
-        }
-        int a = (int) (realFilter / 80f * 180);
-        int r = (int) (200 - (realFilter / 80f) * 190);
-        int g = (int) (180 - (realFilter / 80f) * 170);
-        int b = (int) (60 - realFilter / 80f * 60);
-        return Color.argb(a, r, g, b);
     }
 
 }
